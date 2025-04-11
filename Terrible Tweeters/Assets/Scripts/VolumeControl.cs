3e32d9cbcca5d4817110c61dd2f12c398c108ae3
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-04-11 01:18:47 -0700

    WE HAVE VOLUME CONTROLS (yippee)

--- Terrible Tweeters/Assets/Scripts/VolumeControl.cs
+++ Terrible Tweeters/Assets/Scripts/VolumeControl.cs
@@ -7,12 +7,13 @@ using UnityEngine.UI;
 
 public class VolumeControl : MonoBehaviour
 {
-    [SerializeField] string _volumeParamter = "MasterVolume";
+    [SerializeField] string _volumeParameter = "MasterVolume";
     [SerializeField] AudioMixer _mixer;
     [SerializeField] Slider _slider;
     [SerializeField] float _multiplier = 30f;
     [SerializeField] Toggle _toggle;
     private bool _disableToggleEvent;
+    private float _previousVolume = 1f;
 
     private void Awake()
     {
@@ -29,23 +30,31 @@ public class VolumeControl : MonoBehaviour
 
         if (enableSound)
         {
-            _slider.value = _slider.maxValue;
+            _slider.value = _previousVolume; //restore previous volume
         }
 
         else
         {
+            _previousVolume = _slider.value; //save current volume
             _slider.value = _slider.minValue;
         }
 
     }
     private void OnDisable()
     {
-        PlayerPrefs.SetFloat(_volumeParamter, _slider.value);
+        PlayerPrefs.SetFloat(_volumeParameter, _slider.value);
     }
 
     private void HandleSliderValueChanged(float value)
     {
-        _mixer.SetFloat(_volumeParamter, Mathf.Log10(value) * _multiplier);
+        if(value == _slider.minValue)
+        {
+            _mixer.SetFloat(_volumeParameter, -80f);
+        }
+        else
+        {
+            _mixer.SetFloat(_volumeParameter, Mathf.Log10(value) * _multiplier);
+        }
         _disableToggleEvent = true;
         _toggle.isOn = _slider.value > _slider.minValue;
         _disableToggleEvent = false;
@@ -53,7 +62,7 @@ public class VolumeControl : MonoBehaviour
 
     void Start()
     {
-        _slider.value = PlayerPrefs.GetFloat(_volumeParamter, _slider.value);
+        _slider.value = PlayerPrefs.GetFloat(_volumeParameter, _slider.value);
     }
 
     // Update is called once per frame
